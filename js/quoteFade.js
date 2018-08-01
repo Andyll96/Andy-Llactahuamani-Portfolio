84250e40d98a35a75233631a302b797d90964fd0
Andy <andy.llactahuamani@ryerson.ca>
2018-08-01 02:46:52 -0400

    Quote Fade In fully working

--- js/quoteFade.js
+++ js/quoteFade.js
@@ -1,4 +1,4 @@
-function splitWords(){
+function splitWords() {
     let quote = document.querySelector("blockquote");
     console.log(quote.innerHTML);
 
@@ -10,7 +10,7 @@ function splitWords(){
 
     for (let i = 0; i < wordCount; i++) {
         quote.innerHTML += "<span>" + words[i] + "</span>";
-        if ( i < wordCount - 1) {
+        if (i < wordCount - 1) {
             quote.innerHTML += " ";
         }
     }
@@ -21,24 +21,52 @@ function splitWords(){
     //console.log(quote.innerHTML);
 }
 
-function fadeWords(words){
+function fadeWords(words) {
 
-    words.forEach(function(word){
+    words.forEach(function (word) {
         word.animate([
-            {opacity: 0},
-            {opacity: 1}
+            { opacity: 0, filter: "blur("+getRand(2,4)+"px)" },
+            { opacity: 1, filter: "blur(0px)" }
         ],
-        {
-            duration: 1000,
-            easing: 'ease-in-out',
-            delay: getRand(500, 3300),
-            fill: 'forwards'
-        }
-    )});
+            {
+                duration: 1000,
+                easing: 'ease-in-out',
+                delay: getRand(500, 3300),
+                fill: 'forwards'
+            }
+        )
+    });
 }
 
 function getRand(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-splitWords();
+function isInViewport(elem) {
+    let bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+let quote = document.querySelector("blockquote");
+let cite = document.querySelector("cite");
+let triggered = false;
+
+window.addEventListener('scroll', function (e) {
+    if (isInViewport(quote) && triggered == false) {
+        splitWords();
+        cite.animate([
+            { opacity: 0, filter: "blur("+getRand(2,4)+"px)" },
+            { opacity: 1, filter: "blur(0px)" }
+        ],{
+            duration: 1000,
+            easing: 'ease-in-out',
+            delay: 4000,
+            fill: 'forwards'
+        })
+        triggered = true;
+    }
+}, false);
