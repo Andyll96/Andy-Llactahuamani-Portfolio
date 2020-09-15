2fa0e9a2269c886498da1e8d47eed479885f0b48
Andy Llactahuamani - Surface <andy.llactahuamani@ryerson.ca>
2020-09-14 23:40:35 -0400

    Sections given to home page

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -3,7 +3,24 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <section>
+                <h1>Andy Llactahuamani</h1>
+            </section>
+            <section>
+                <h1>Quote</h1>
+            </section>
+            <section>
+                <h1>Photos</h1>
+            </section>
+            <section>
+                <h1>Film</h1>
+            </section>
+            <section>
+                <h1>Code</h1>
+            </section>
+            <footer>
+                <h1>Footer</h1>
+            </footer>
         </div>
     )
 }
