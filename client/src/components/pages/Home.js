d99b4f9bd5169682ad216eedc7f569b0c2d7c4c2
Andy Llactahuamani - Surface <andy.llactahuamani@ryerson.ca>
2020-09-16 21:04:51 -0400

    section height

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -9,30 +10,49 @@ const Home = () => {
 
                 <div>
                     <a href='https://www.github.com/andyll96' target='_blank'>
-                        Github
+                        <i class="fab fa-github"></i>
                     </a>
                     <a href='https://www.linkedin.com/in/andy-llactahuamani-6386b5164/' target='_blank'>
-                        LinkedIn
+                        <i class="fab fa-linkedin-in"></i>
                     </a>
                     <a href='https://www.instagram.com/andysll' target='_blank'>
-                        Instagram
+                        <i class='fab fa-instagram' />
                     </a>
                 </div>
             </section>
             <section>
-                <h1>Quote</h1>
+                <quote>"The audience knows the truth. The world is simple. It's miserable. Solid all the way through. But if you could fool them, even for a second... Then you got to see something very special"</quote>
+                <h3>~The Prestige</h3>
+                
             </section>
             <section>
                 <h1>Photos</h1>
+                <Link to='/photos'>View Photos</Link>
             </section>
             <section>
                 <h1>Film</h1>
+                <Link to='/film'>View Films</Link>
+                {/* This will link to the project page of the currently showing project */}
+                {/* <Link to='/films/'>View Project</Link> */}
             </section>
             <section>
                 <h1>Code</h1>
+                {/* <Link to='/code/d'>View Project</Link> */}
+                <Link to='/code'>View More</Link>
             </section>
             <footer>
-                <h1>Footer</h1>
+                <h1>Something Inspirational</h1>
+                <div>
+                    <a href='https://www.github.com/andyll96' target='_blank'>
+                        <i class="fab fa-github"></i>
+                    </a>
+                    <a href='https://www.linkedin.com/in/andy-llactahuamani-6386b5164/' target='_blank'>
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+                    <a href='https://www.instagram.com/andysll' target='_blank'>
+                        <i class='fab fa-instagram' />
+                    </a>
+                </div>
             </footer>
         </div>
     )
