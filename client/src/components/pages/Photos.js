a30996631c24685c0a8ee27ae2976957dd3e0da4
Andy Llactahuamani - Surface <andy.llactahuamani@ryerson.ca>
2020-10-30 22:20:23 -0400

    photography default description

--- client/src/components/pages/Photos.js
+++ client/src/components/pages/Photos.js
@@ -6,13 +6,17 @@ const Photos = () => {
             <div className="container photo-header px-5">
                 <div className='container'>
                 <h1>Photography</h1>
-                <p className='album-description px-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur atque natus amet? Ea dolorem, at consectetur atque, commodi voluptatum nemo possimus dicta minima nostrum inventore exercitationem aliquid sed. Placeat, neque.Molestias deserunt itaque similique voluptatum ducimus facere aliquam hic voluptates officiis, ratione harum minus aut commodi repudiandae at perspiciatis provident corporis et sunt dolorum quo? Autem possimus saepe dolor suscipit.</p>
+                    <p className='album-description px-3'>I often find it difficult to maintain a certain level of creativity in my life. I find that the more time I spend away from being expressive, whether it be neglect in favor of certain responsibilities or lack of motivation, the harder it becomes to get in the mindset to be original and imaginative. It's the inertia of life that wants us to stay comfortable and static. But photography is inspiring to me, it's technicality and composition. It's not comfortable, it's exciting and fun. It's not static, it's challenging.
+                    <br/>
+                    <br/>
+                    This is a collection of my work. I hope you enjoy it as much as I did creating it.  
+                </p>
                 </div>
                 <div className="album-titles">
-                    <h2 className='current'>City</h2>
+                    {/* <h2 className='current'>City</h2>
                     <h2>Night</h2>
                     <h2>Neon</h2>
-                    <h2>Water</h2>
+                    <h2>Water</h2> */}
                 </div>
             </div>
             <div className="photo-gallery">
