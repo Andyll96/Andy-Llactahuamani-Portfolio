674a7dea1a6c6e33473626243103f68b1655a58b
Andy Llactahuamani - Surface <andy.llactahuamani@ryerson.ca>
2020-10-31 04:56:15 -0400

    fixed general positioning for description in photo page. Must populate with albums and categories

--- client/src/components/pages/Photos.js
+++ client/src/components/pages/Photos.js
@@ -2,21 +2,23 @@ import React from 'react'
 
 const Photos = () => {
     return (
-        <div>
+        <div className='below-nav'>
             <div className="container photo-header px-5">
                 <div className='container'>
-                <h1>Photography</h1>
+                    <h1>Photography</h1>
                     <p className='album-description px-3'>I often find it difficult to maintain a certain level of creativity in my life. I find that the more time I spend away from being expressive, whether it be neglect in favor of certain responsibilities or lack of motivation, the harder it becomes to get in the mindset to be original and imaginative. It's the inertia of life that wants us to stay comfortable and static. But photography is inspiring to me, it's technicality and composition. It's not comfortable, it's exciting and fun. It's not static, it's challenging.
-                    <br/>
-                    <br/>
-                    This is a collection of my work. I hope you enjoy it as much as I did creating it.  
+                    <br />
+                        <br />
+                    This is a collection of my work. I hope you enjoy it as much as I did creating it.
                 </p>
                 </div>
+
+                {/* TODO: RENAME TO PHOTO-NAV */}
                 <div className="album-titles">
-                    {/* <h2 className='current'>City</h2>
-                    <h2>Night</h2>
-                    <h2>Neon</h2>
-                    <h2>Water</h2> */}
+                    <a href="" className='current'><h2 className='button'>City</h2></a>
+                    <a href=""><h2 className='button'>Night</h2></a>
+                    <a href=""><h2 className='button'>Neon</h2></a>
+                    <a href=""><h2 className='button'>Water</h2></a>
                 </div>
             </div>
             <div className="photo-gallery">
