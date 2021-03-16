3c6c8841f17dab3a3120d1280d24c2322e31c60f
allactah-Desktop <andy.llactahuamani@ryerson.ca>
2021-03-16 02:07:56 -0400

    styling of currentImage fixed. Must add carosel and animations

--- client/src/components/layout/CurrentImageItem.js
+++ client/src/components/layout/CurrentImageItem.js
@@ -3,9 +3,9 @@ import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
 import { connect } from "react-redux";
 import { setCurrentImage, clearCurrentImage } from "../../actions/photosActions";
 
-const CurrentImageItem = ({photos: {images, filteredImages, currentImage}, clearCurrentImage}) => {
+const CurrentImageItem = ({ photos: { images, filteredImages, currentImage }, clearCurrentImage }) => {
 
-    
+
     useEffect(() => {
         // TODO: THIS MAY HAVE TO CHANGE DEPENDING ON HOW THE PAGE LOADS ON DIFFERENT DEVICES
         window.scrollTo(0, 113);
@@ -15,23 +15,44 @@ const CurrentImageItem = ({photos: {images, filteredImages, currentImage}, clear
         clearCurrentImage();
     }
 
-    document.addEventListener('keydown', function(event){
-        if(event.keyCode == 27)
+    document.addEventListener('keydown', function (event) {
+        if (event.keyCode == 27)
             exitCurrentImage();
-        if(event.keyCode == 70)
+        if (event.keyCode == 70)
             document.querySelector('.current-image').requestFullscreen().catch((e) => {
                 console.log(e);
             });
     })
 
     return (
-        <motion.div className="full-page below-nav">
-            <button onClick={() => exitCurrentImage()}>EXIT</button>
+        <motion.div className="current-image-page below-nav">
+            <a className="exit-button" onClick={() => exitCurrentImage()}>
+                <i class="fas fa-times fa-3x"></i>
+            </a>
+
             <div>
                 <img className="current-image" src={currentImage.fileLocation} ></img>
             </div>
-            <div>
 
+            <div className="current-image-info">
+                <div>
+                    <h3 className="current-image-camera">{currentImage.camera}</h3>
+                </div>
+                <div>
+                    <h3 className="current-image-camera">{currentImage.focalLength}mm</h3>
+                </div>
+                <div>
+                    <h3 className="current-image-camera">f/{currentImage.fStop}</h3>
+                </div>
+                <div>
+                    <h3 className="current-image-camera">{currentImage.shutterSpeed}</h3>
+                </div>
+                <div>
+                    <h3 className="current-image-camera">ISO {currentImage.iso}</h3>
+                </div>
+            </div>
+            <div>
+                {/* Carosel */}
             </div>
         </motion.div>
     )
@@ -41,4 +62,4 @@ const mapStateToProps = state => ({
     photos: state.photos
 });
 
-export default connect(mapStateToProps, {setCurrentImage, clearCurrentImage})(CurrentImageItem)
+export default connect(mapStateToProps, { setCurrentImage, clearCurrentImage })(CurrentImageItem)
